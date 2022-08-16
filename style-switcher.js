22335ca29232155f829a35e7fd1af181293a50ce
SharmaYashasvi <202011079@diu.iiitvadodara.ac.in>
2022-08-16 23:59:54 +0530

    my profile

---
+++ style-switcher.js
@@ -0,0 +1,50 @@
+    // toggal style switcheer
+
+   const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+   styleSwitcherToggler.addEventListener("click" , () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+   })
+
+//    hide style switch on scroll
+
+window.addEventListener("scroll" , () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open") ;
+    }
+})
+
+// theme clors
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled" , "true");
+        }
+    })
+}
+
+// day-night
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
